f6240ba8996c7c74d688ebc985e0dd5cc1599ac0
Li Tao <li.tao@circle.com>
2025-02-19 17:43:38 -0800

    chore(No Story): Describe the security issue in the PR

--- permissions/check.ts
+++ permissions/check.ts
@@ -15,15 +15,15 @@ export enum Roles {
       CAN_GENERATE_MONTHLY_REPORT: true,
     },
     [Roles.USER]: {
-      CAN_VIEW_SENSITIVE_DATA: true,
-      CAN_DELETE_USERS: false,
+      CAN_VIEW_SENSITIVE_DATA: true, // user should not be able to view sensitive data
+      CAN_DELETE_USERS: false, // not sure how user delete its own account
       CAN_VIEW_TRANSACTION_HISTORY: true,
       CAN_VIEW_ACCOUNT_SETTINGS: true,
       CAN_VIEW_ALL_OUTSTANDING_BALANCES: false,
       CAN_GENERATE_MONTHLY_REPORT: false,
     },
     [Roles.GUEST]: {
-      CAN_VIEW_SENSITIVE_DATA: false, 
+      CAN_VIEW_SENSITIVE_DATA: false, // guest have all permisison to false, not sure what guest can do here
       CAN_DELETE_USERS: false,
       CAN_VIEW_TRANSACTION_HISTORY: false,
       CAN_VIEW_ACCOUNT_SETTINGS: false,
@@ -33,7 +33,7 @@ export enum Roles {
   };
   
 
-  export function permissionCheck(
+  export function permissionCheck( // doing permission check on FE alone is not safe at all
     permission: keyof typeof RolePermissions[Roles.ADMIN],
     session?: { role?: Roles }
   ): boolean {
